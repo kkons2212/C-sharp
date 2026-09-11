c4c10464c6eaeb90856f6a3a020ebb4ec602e0e3
Quocbao <thaidangquocbao@gmail.com>
2026-09-11 16:21:33 +0700

    Tuan1/Bai1/Ma-nguon-chuong-trinh

---
+++ Tuan1/Bai1/bai1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Tuan1
+{
+    public class Bai1
+    {
+        public static void Run()
+        {
+            Console.WriteLine("nhap ho ten");
+            string name = Console.ReadLine();
+            Console.Write("xin chao"+ name);
+        
+        }
+    }
+}
