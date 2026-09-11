4773a43b1e4fdb0e29f590872d9c3647c2d472fd
Quocbao <thaidangquocbao@gmail.com>
2026-09-11 21:25:42 +0700

    Tuan1/Bai5/Menu

---
+++ Tuan1/Bai5/Bai5.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Tuan1
+{
+    public class Bai5
+    {
+        public static void Run()
+        {       int chon;
+                double x=0,y=0;
+                while(true)
+            {
+                Console.WriteLine("MENU");
+                Console.WriteLine("1. Nhap hai gia tri so thuc cho x, y");   
+                Console.WriteLine("2. Tinh x^y");
+                Console.WriteLine("3. Tinh can bac 2 cua x va y");
+                Console.WriteLine("4. Thoat");  
+                Console.Write("Chon chuc nang:"); 
+                chon = Convert.ToInt32(Console.ReadLine());
+                switch(chon)
+                {
+                    case 1:
+                    Console.WriteLine("Nhap x:");
+                    x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Nhap y:");
+                    y = Convert.ToDouble(Console.ReadLine());
+                    break;
+                    case 2:
+                    if(x==0||y==0)
+                        {
+                            Console.Write("chua nhap gia tri x va y");
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write($"{x} mu {y} la:"+Math.Pow(x,y));
+                            
+                        }
+                    break;
+                    case 3:
+                     if(x==0||y==0)
+                        {
+                            Console.Write("chua nhap gia tri x va y");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"can bac 2 cua {x} la:"+Math.Sqrt(x));
+                            Console.Write($"can bac 2 cua {y} la:"+Math.Sqrt(y));
+                        }
+                    break;
+                    case 4:
+                    break;
+                    default:
+                    Console.WriteLine("vui long nhap so hop le");
+                    break;
+                }
+            }
+        }
+   }
+}
--- Tuan1/Program.cs
+++ Tuan1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Tuan1;
-
 //Bai1.Run();
 //Bai2.Run();
 //Bai3.Run();
-Bai4.Run();
+//Bai4.Run();
+Bai5.Run();
